8bf916ba80938e960df0bb340d78999b3c69be1d
TobyComon <toby.comon@gmail.com>
2022-03-28 15:20:01 -0600

    Initial

---
+++ main.js
@@ -0,0 +1,34 @@
+let team1 = 0
+let team2 = 0
+
+
+function team1Score(score) {
+    
+    team1 += score
+
+    console.log(team1)
+    drawScores()
+}
+
+function team2Score() {
+    team2++
+    console.log(team2)
+    drawScores()
+}
+
+function drawScores() {
+   let team1ScoreElem =  document.getElementById("team1Score")
+
+   let team2ScoreElem =  document.getElementById("team2Score")
+
+   team1ScoreElem.innerText = (team1)
+   team2ScoreElem.innerText = (team2)
+}
+
+function clearScore(teamName) {
+    team1 = 0
+    team2 = 0
+    drawScores()
+}
+
+drawScores()
